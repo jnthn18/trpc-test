b7dfacc48e58429ada454587dbd70476dface7f5
Jonathan More <jnthn18@gmail.com>
2025-02-23 10:32:18 -0700

    Refactor hydration and prefetch

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,12 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getQueryClient, trpc } from "@/trpc/server";
+import { HydrateClient, trpc, prefetch } from "@/trpc/server";
 import { ClientGreeting } from "./client-greeting";
 
 export default async function TestPage() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.user.hello.queryOptions({ text: "Test" }),
-  );
+  prefetch(trpc.user.hello.queryOptions({ text: "Test" }));
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
+    <HydrateClient>
       <ClientGreeting />
-    </HydrationBoundary>
+    </HydrateClient>
   );
 }
--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import "server-only";
 
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
@@ -6,8 +7,30 @@ import { makeQueryClient } from "./query-client";
 import { appRouter } from "@/server/root";
 import { createTRPCContext } from "@/server/trpc";
 import { headers } from "next/headers";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import type { TRPCQueryOptions } from "@trpc/tanstack-react-query";
 
 export const getQueryClient = cache(makeQueryClient);
+
+export function HydrateClient(props: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {props.children}
+    </HydrationBoundary>
+  );
+}
+
+export function prefetch<T extends ReturnType<TRPCQueryOptions<any>>>(
+  queryOptions: T,
+) {
+  const queryClient = getQueryClient();
+  if (queryOptions.queryKey[1]?.type === "infinite") {
+    void queryClient.prefetchInfiniteQuery(queryOptions as any);
+  } else {
+    void queryClient.prefetchQuery(queryOptions);
+  }
+}
 
 const createContext = cache(async () => {
   const heads = new Headers(await headers());

554b419a999442414f5f16e5e0e3599c3e4affce
Jon <jnthn18@gmail.com>
2025-02-24 08:25:48 -0700

    Add comment router

--- src/server/root.ts
+++ src/server/root.ts
@@ -1,8 +1,10 @@
 import { postRouter } from "./routers/post-router";
+import { commentRouter } from "./routers/comment-router";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  comment: commentRouter,
 });
 
 export type AppRouter = typeof appRouter;

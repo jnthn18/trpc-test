0c0d1b7633a2c15537dc30774d5285243fd11174
Jon <jnthn18@gmail.com>
2025-02-23 22:56:26 -0700

    Create post page outline and posts list styling

---
+++ src/app/posts/create/page.tsx
@@ -0,0 +1,7 @@
+export default async function CreatePostPage() {
+  return (
+    <div className="mt-8 flex justify-center">
+      <h1 className="text-4xl font-bold">Create Post</h1>
+    </div>
+  );
+}
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -10,17 +10,23 @@ export default async function PostsPage() {
   const posts = await caller.post.list();
 
   return (
-    <div className="min-h-screen text-center space-y-4 p-8">
-      <h1 className="text-4xl font-bold">Posts Page</h1>
+    <div className="min-h-screen text-center gap-8 p-8 flex flex-col max-w-80 mx-auto items-center">
+      <h1 className="text-4xl font-bold">Posts</h1>
       {posts.map((post) => (
         <Link
           key={post.id}
           href={`/posts/${post.id}`}
-          className="text-lg font-semibold"
+          className="text-2xl font-semibold hover:underline"
         >
           {post.title}
         </Link>
       ))}
+      <Link
+        href="/posts/create"
+        className="border rounded-lg py-3 px-4 text-lg font-bold hover:text-background hover:bg-foreground"
+      >
+        Create Post
+      </Link>
     </div>
   );
 }
